1f002433119fa82d8b38670ff022a7ffa4d8c7c9
N.pushkar <pushkar.anastasiia@gmail.com>
2024-06-30 23:57:40 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import Game, {GameSavingData, readGameSaving as loadgame, writeGameSaving as saveGame} from "./game.js";
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+import Character from './domain.js'
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+
+  export default Game;
+  
+  export class GameSavingData {
+  }
+
+  
+ export function readGameSaving() {
+  }
+  
+export function writeGameSaving() {
+  }
+
+  
